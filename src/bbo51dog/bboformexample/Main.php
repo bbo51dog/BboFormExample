ec435dff497e98ebdceb3b23aac28099d8c94a55
bbo51dog <bbo51dog@gmail.com>
2021-04-27 23:08:09 +0900

    v1.0.0

---
+++ src/bbo51dog/bboformexample/Main.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace bbo51dog\bboformexample;
+
+use bbo51dog\bboform\element\Button;
+use bbo51dog\bboform\element\ClosureButton;
+use bbo51dog\bboform\element\Dropdown;
+use bbo51dog\bboform\element\Input;
+use bbo51dog\bboform\element\Label;
+use bbo51dog\bboform\element\Slider;
+use bbo51dog\bboform\element\StepSlider;
+use bbo51dog\bboform\element\Toggle;
+use bbo51dog\bboform\form\ClosureCustomForm;
+use bbo51dog\bboform\form\CustomForm;
+use bbo51dog\bboform\form\SimpleForm;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onJoin(PlayerJoinEvent $event) {
+        $nameInput = new Input("What is your name?", "name", "Steve");
+        $form = (new ClosureCustomForm(function (Player $player, CustomForm $form) use ($nameInput) {
+            $welcomeForm = (new SimpleForm())
+                ->setText("Welcome to our server, {$nameInput->getValue()}!!\n")
+                ->addButton(new ClosureButton("Done", null, function (Player $player, Button $button) {
+                    $player->sendMessage("Please enjoy!");
+                }));
+            $player->sendForm($welcomeForm);
+        }))
+            ->setTitle("Example Form")
+            ->addElement(new Label("This is the example form!!"))
+            ->addElement($nameInput)
+            ->addElement(new Slider("How old are you?", 0, 100, 18))
+            ->addElement(new StepSlider("Choose your gender", ["Male", "Female", "Others"], 2))
+            ->addElement(new Dropdown("Where are you from?", ["Japan", "U.S.", "Others"]))
+            ->addElement(new Toggle("Do you agree to the Terms of Use?"));
+        $event->getPlayer()->sendForm($form);
+    }
+}
